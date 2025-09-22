8417a5134afbee40d4ff88eb9fa18a79282cec8b
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2025-09-22 14:24:46 +0200

    save

---
+++ Hálózat progi/Programok/09.22-gyakorlo.py
@@ -0,0 +1,34 @@
+# #1. feladat
+# szam = int(input("Írj be egy pozitív egész számot: "))
+# print(f"A szám kétszerese {szam*2}")
+# print(f"A szám heted része {round(szam/7,2)}")
+# print(f"3-mal osztva az egészrész: {szam//3} maradék: {szam%3}")
+# if szam % 2 == 0:
+#     print("A szám páros")
+# else:
+#     print("A szám páratlan")
+
+
+
+
+# #2. feladat
+# print(f"{type('anya')},{type(5)},{type(3.14)},{type([1,2,3])}")
+
+
+
+# #3. feladat
+# lista = []
+# szam = 0
+# for i in range(15):
+#     szam += 1
+#     lista.append(str(szam))
+# print(', '.join(lista))
+
+# lista2 = []
+# szamocska = 0
+# while True:
+#     szamocska += 1
+#     lista2.append(szamocska)
+#     if szamocska == 15:
+#         break
+# print(', '.join(str(lista2)))
