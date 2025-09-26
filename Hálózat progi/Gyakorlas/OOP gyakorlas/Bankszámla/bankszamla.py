b9d7e16e3026bc6450faa7c5d492288d88b370ce
halasznoel <halnoel1234@gmail.com>
2025-09-27 00:59:20 +0200

    save09.27

--- Hálózat progi/Gyakorlas/OOP gyakorlas/Bankszámla/bankszamla.py
+++ Hálózat progi/Gyakorlas/OOP gyakorlas/Bankszámla/bankszamla.py
@@ -1,3 +1,6 @@
+import tkinter as tk
+from tkinter import Frame, messagebox
+
 class Bankszámla:
     def __init__(self,nev,egyenleg):
         self.nev = nev
@@ -6,42 +9,114 @@ class Bankszámla:
     def befizetes(self,osszeg):
         if osszeg > 0:
             self.egyenleg += osszeg
-            print(f"Sikeres befizetés! Új egyenleg: {self.egyenleg} Ft")
+            return True
         else:
-            print("A befizetés csak pozitív összeg lehet.")
+            return False
     
     def kivetel(self,osszeg):
         if osszeg > 0 and self.egyenleg >= osszeg:
             self.egyenleg -= osszeg
-            print(f"Sikeres kivétel! Új egyenleg: {self.egyenleg} Ft")
+            return True
         else:
-            print("Sajnos nincs elég fedezet a tranzakcióhoz.")
+            return False
     
     def egyenleg_lekerdezes(self):
-        print(f"Jelenleg ennyi forint van a számládon: {self.egyenleg} Ft")
+        return f"Jelenleg ennyi forint van a számládon: {self.egyenleg} Ft"
 
-szamla1 = Bankszámla('Halász István',25000000)
 
-print("Üdv a Halászos Bankban \n Írd le, hogy mit szeretnél csinálni")
 
-while True:
-    muvelet = input("(egyenleg)\n(befizetés)\n(kivétel)\n(kilépés)\nAdd meg a műveletet: ")
-    if muvelet == "egyenleg":
-        szamla1.egyenleg_lekerdezes()
-    elif muvelet == "befizetés":
-        try:
-            osszeg = int(input("Add meg az összeget amit be szeretnél fizetni: "))
-            szamla1.befizetes(osszeg)
-        except ValueError:
-            print("Hiba: Kérlek, számot adj meg!")
-    elif muvelet == "kivétel":
-        try:
-            osszeg = int(input("Add meg az összeget amit fel szeretnél venni: "))
-            szamla1.kivetel(osszeg)
-        except ValueError:
-            print("Hiba: Kérlek, számot adj meg!")
-    elif muvelet == "kilépés":
-        print("Viszlát!")
-        break
+
+def perform_befizetes(amount):
+    if szamla1.befizetes(amount):
+        egyenleg_label.config(text=szamla1.egyenleg_lekerdezes())
+
+
+def perform_kivetel(amount):
+    if szamla1.kivetel(amount):
+        egyenleg_label.config(text=szamla1.egyenleg_lekerdezes())
     else:
-        print("Ismeretlen művelet. Kérlek, próbáld újra!")
+        messagebox.showwarning(title="Hiba", message="Elfogyott a pénzed :)")
+    
+
+
+
+def show_main_frame():
+    befizetes_frame.pack_forget()
+    main_frame.pack()
+    kivetel_frame.pack_forget()
+
+def show_befizetes_frame():
+    main_frame.pack_forget()
+    befizetes_frame.pack()
+    kivetel_frame.pack_forget()
+
+def show_kivetel_frame():
+    main_frame.pack_forget()
+    befizetes_frame.pack_forget()
+    kivetel_frame.pack()
+
+
+# GUI
+app = tk.Tk()
+app.title("Halászos Bank") # Ablak neve
+app.geometry("800x600") # Ablak mérete
+szamla1 = Bankszámla('Halász István',50000)
+
+main_frame = tk.Frame(app)
+
+
+# Egyenleg label
+egyenleg_text = szamla1.egyenleg_lekerdezes() # Eltárolom egy változóba a számla adatait
+egyenleg_label = tk.Label(app, text=egyenleg_text, font=("Poppins", 16)) # Maga a label-be belehelyezem az eltárolt adatot
+egyenleg_label.pack(pady=20) # Eligazítom az ablakon belül
+
+
+
+# Pénzbefizetés gombok
+befizetes_frame = tk.Frame(app)
+
+befizetes_button = tk.Button(main_frame, text="Befizetés", command=show_befizetes_frame)
+befizetes_button.pack(pady=20)
+
+befizetes_5000_button = tk.Button(befizetes_frame, text="5 000 Ft", command=lambda:perform_befizetes(5000), font=("Poppins", 10))
+befizetes_5000_button.pack(pady=20)
+
+befizetes_10000_button = tk.Button(befizetes_frame, text="10 000 Ft", command=lambda:perform_befizetes(10000), font=("Poppins", 10))
+befizetes_10000_button.pack(pady=20)
+
+befizetes_20000_button = tk.Button(befizetes_frame, text="20 000 Ft", command=lambda:perform_befizetes(20000), font=("Poppins", 10))
+befizetes_20000_button.pack(pady=20)
+
+
+
+
+
+# Pénzkivétel gombok
+kivetel_frame = tk.Frame(app)
+
+kivetel_button = tk.Button(main_frame, text="Kivétel", command=show_kivetel_frame)
+kivetel_button.pack(fill=tk.X,pady=20)
+
+
+kivetel_5000_button = tk.Button(kivetel_frame, text="5 000 Ft", command=lambda:perform_kivetel(5000), font=("Poppins", 10))
+kivetel_5000_button.pack(pady=20)
+
+kivetel_10000_button = tk.Button(kivetel_frame, text="10 000 Ft", command=lambda:perform_kivetel(10000), font=("Poppins", 10))
+kivetel_10000_button.pack(pady=20)
+
+kivetel_20000_button = tk.Button(kivetel_frame, text="20 000 Ft", command=lambda:perform_kivetel(20000), font=("Poppins", 10))
+kivetel_20000_button.pack(pady=20)
+
+
+
+# Vissza gomb
+back_button = tk.Button(befizetes_frame, text="Vissza", command=show_main_frame, font=("Poppins", 10))
+back_button.pack(pady=20)
+
+back_button = tk.Button(kivetel_frame, text="Vissza", command=show_main_frame, font=("Poppins", 10))
+back_button.pack(pady=20)
+
+
+show_main_frame() # Ez mutatja a főablakot
+app.mainloop() # Ez tartja megnyitva az ablakot
+
