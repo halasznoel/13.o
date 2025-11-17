1420c15935dc69a85b6abf7ac6e74b125af094ad
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2025-11-17 12:26:54 +0100

    save-11.17

---
+++ Hálózat progi/Programok/11/telnet2.py
@@ -0,0 +1,29 @@
+from netmiko import ConnectHandler
+
+device = {
+    'device_type': 'cisco_ios_telnet',
+    'ip': '10.1.102.160',
+    'username': 'admin',
+    'password': 'cisco',
+    'secret': 'cisco',
+}
+
+net_connect = None
+
+try:
+    net_connect=ConnectHandler(**device)
+    net_connect.enable()
+    print("Telnet login successful!")
+    output = net_connect.send_command("show version")
+    print("\nOutput of 'show version':\n")
+    print(output)
+except Exception as e:
+    print(f"Telnet login failed: {str(e)}")
+
+finally:
+    try:
+        if net_connect:
+            net_connect.disconnect()
+            print("Disconnected from the device.")
+    except NameError:
+        pass
