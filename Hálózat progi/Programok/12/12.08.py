064858c0db432fdc8489f0e6c99a9a8403ace0e5
Halász Noel <halasz.noel.2021e@ms.gdszeged.hu>
2025-12-08 14:15:06 +0100

    save

---
+++ Hálózat progi/Programok/12/12.08.py
@@ -0,0 +1,51 @@
+import paramiko
+import time
+
+def cisco_login(hostname, username, password, enable_password):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    try:
+        ssh.connect(hostname, username=username, password=password, allow_agent=False, look_for_keys=False)
+        channel = ssh.invoke_shell()
+        time.sleep(1)
+        output = channel.recv(65535).decode('utf-8')
+        print(output)
+        if '#' in output or '>' in output:
+            channel.send("enable\n")
+            time.sleep(1)
+            channel.send(enable_password+"\n")
+            time.sleep(1)
+            enable_output = channel.recv(65535).decode('utf-8')
+            print(enable_output)
+            if '#' in enable_output:
+                print('Login successful! Entered privilege mode')
+            else:
+                print("Enable mode failed!")
+                print("Login failed")
+        else:
+            print("Login failed")
+        channel.send("configure terminal\n")
+        time.sleep(1)
+        config_output = channel.recv(65535).decode('utf-8')
+        print(config_output)
+        if '(config)' in config_output:
+            print("Entered configurate mode")
+            for interface_number in range(10,15):
+                channel.send("interface gi1/0/%s" % interface_number + "\n")
+                channel.send("shutdown\n")
+                channel.send("exit\n")
+                print("interface gi1/0/%s" % interface_number + "was shut down\n")
+                channel.send("end\n")
+            print("Entered privilege mode")
+        ssh.close()
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Login failed!")
+if __name__ == "__main__":
+    device_hostname = "10.1.102.160"
+    device_username = "admin"
+    device_password = "cisco"
+    device_enable_password = "cisco"
+
+
+cisco_login(device_hostname, device_username, device_password,device_enable_password)
